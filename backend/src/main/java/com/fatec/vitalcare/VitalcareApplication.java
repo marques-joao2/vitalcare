dc171f131075b815753522082ffb598ab475a23d
Joao Marques <joao.pereira92@fatec.sp.gov.br>
2024-09-09 19:11:18 -0300

    chore: add estrutura inicial do spring boot

---
+++ backend/src/main/java/com/fatec/vitalcare/VitalcareApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.vitalcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalcareApplication.class, args);
+	}
+
+}
