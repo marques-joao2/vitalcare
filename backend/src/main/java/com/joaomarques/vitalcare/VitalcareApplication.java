07820b1cba543fe6bef466f48606a0753849424d
Joao Marques <joao.pereira92@fatec.sp.gov.br>
2024-10-06 18:40:17 -0300

    refactor: inclui novo backend

--- backend/src/main/java/com/joaomarques/vitalcare/VitalcareApplication.java
+++ backend/src/main/java/com/joaomarques/vitalcare/VitalcareApplication.java
@@ -1,4 +1,4 @@
-package com.fatec.vitalcare;
+package com.joaomarques.vitalcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/joaomarques/vitalcare/VitalcareApplicationTests.java
+++ backend/src/test/java/com/joaomarques/vitalcare/VitalcareApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fatec.vitalcare;
+package com.joaomarques.vitalcare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
